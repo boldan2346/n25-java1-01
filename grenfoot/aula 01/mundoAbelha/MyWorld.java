a113875718e151f4f2c0a6e90fc1d10cf50f5e30
helena <helenaboldan@gmail.com>
2025-06-03 21:52:05 -0300

    Abelha

---
+++ grenfoot/aula 01/mundoAbelha/MyWorld.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Aranha aranha = new Aranha();
+        addObject(aranha,263,163);
+        aranha.setLocation(30,38);
+        Abelha abelha = new Abelha();
+        addObject(abelha,65,161);
+        Mosca mosca = new Mosca();
+        addObject(mosca,42,360);
+        Mosca mosca2 = new Mosca();
+        addObject(mosca2,76,289);
+        Mosca mosca3 = new Mosca();
+        addObject(mosca3,136,344);
+        Mosca mosca4 = new Mosca();
+        addObject(mosca4,165,258);
+    }
+}
